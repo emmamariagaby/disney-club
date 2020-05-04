6ad77e244ee6ef36618928eb70e94992a651dbf0
Louise Bäckström <backstromlouise@gmail.com>
2020-05-04 17:39:11 +0200

    added mongo.js

---
+++ mongo.js
@@ -0,0 +1,71 @@
+const mongodb = require('mongodb')
+
+
+async function connect() {
+    const client = mongodb.MongoClient
+    const url = "mongodb://localhost:27017/exercise"
+    const options = { useUnifiedTopology: true }
+    const connection = await client.connect(url, options)
+    console.log('Conenction to database has been established')
+    const db = connection.db('exercise')
+    console.log('Database has been created')
+    return db
+}
+
+/**
+ * 
+ * @param {mongodb.Db} db 
+ */
+async function createCollections(db) {
+    const usersCollection = await db.createCollection('users')
+    console.log('Collections has been created')
+    // create all needed collections
+    return [usersCollection]
+}
+
+/**
+ * 
+ * @param {mongodb.Collection} collection 
+ */
+async function insertUser(collection) {
+    const res = await collection.insertOne({
+        name: 'Ellen',
+        password: 's3cr3t'
+    })
+    console.log('A user was created')
+} 
+
+/**
+ * 
+ * @param {mongodb.Collection} collection 
+ */
+async function deleteUsers(collection) {
+    const res = await collection.deleteMany({ name: 'Elias' })
+    console.log('A user was maybe deleted, count: ', res.deletedCount)
+}
+
+/**
+ * 
+ * @param {mongodb.Collection} collection 
+ */
+async function findUsers(collection) {
+    const users = await collection.find({ name: 'Malvina' }).toArray()
+    console.log(users)
+    return users;
+}
+
+
+async function run() {
+    try {
+        const db = await connect()
+        const collections = await createCollections(db)
+        insertUser(collections[0])
+        deleteUsers(collections[0])
+        const users = findUsers(collections[0])
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+run()
