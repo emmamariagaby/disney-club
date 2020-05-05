a8e4817fa722160d805949c925e49313502396cb
Louise Bäckström <backstromlouise@gmail.com>
2020-05-05 11:55:05 +0200

    installed mongoose

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+let app = express();
+app.use(express.json());
+
+
+// Routes
+
+// Koppling till databas
+
+// Port
+app.listen(3000, () => console.log('Server is up and running!'))
