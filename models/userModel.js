4da367d225b651c2de2555b64da6a4cec8315d53
Embla <emblajonsson@gmail.com>
2020-05-06 23:47:37 +0200

    fix

--- models/userModel.js
+++ models/userModel.js
@@ -4,13 +4,15 @@ let UserSchema = {
   userinfo: {
     username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
-    loggedin: Boolean,
-    faveCharacter: String,
-  },
-  posts: {
-    nrOfposts: Number,
-    // posts: /**?? */
   },
 }
+// loggedin: Boolean,
+// faveCharacter: String,
+//   },
+//   posts: {
+//     nrOfposts: Number,
+//     // posts: /**?? */
+//   },
+// }
 
 module.exports = mongoose.model('User', UserSchema)
