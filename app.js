990b1d701b1eb988f76737c01986f908297d51ba
Louise Bäckström <backstromlouise@gmail.com>
2020-05-06 14:47:38 +0200

    rest

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const app = express()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json')
+    next()
+})
+
+
+
 app.listen(3000, () => console.log('Server is up and running!'))
