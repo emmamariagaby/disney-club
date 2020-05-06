f23a6bcf7a01b872779312f0ad6cfc34f340432a
Louise Bäckström <backstromlouise@gmail.com>
2020-05-06 17:26:44 +0200

    added post and get to character.js

--- models/characterModel.js
+++ models/characterModel.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose')
 
-let CharacterSchema = ({
-    name: String,
-    movie: String,
-    bestFrient: String
-})
+let CharacterSchema = {
+    title: {
+      type: String,
+    },
+    info: {
+      name: String,
+      movie: String,
+      bestFrient: String,
+    },
+    description: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+    },
+    img: {
+      data: Buffer,
+      contentType: String,
+    },
+  }
 
 module.exports = mongoose.model("Character", CharacterSchema)
