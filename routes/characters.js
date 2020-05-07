6ff116b9485a120d7be86ec9a2d5cf245e3062fd
Louise Bäckström <backstromlouise@gmail.com>
2020-05-07 13:35:01 +0200

    added PUT

--- routes/characters.js
+++ routes/characters.js
@@ -2,16 +2,8 @@ const express = require('express')
 const characterModel = require('../models/characterModel')
 const router = express()
 
-router.get('/characters', async (req, res) => {
-  const characters = await characterModel.find({})
 
-  try {
-    res.send(characters)
-  } catch (err) {
-    res.status(500).send(err)
-  }
-})
-
+// Create
 router.post('/characters', async (req, res) => {
   const character = new characterModel(req.body)
 
@@ -22,5 +14,40 @@ router.post('/characters', async (req, res) => {
     res.status(500).send(err)
   }
 })
+
+// Read
+router.get('/characters', async (req, res) => {
+    const characters = await characterModel.find({})
+  
+    try {
+      res.send(characters)
+    } catch (err) {
+      res.status(500).send(err)
+    }
+  })
+
+
+// Update
+router.put('/characters/:id', async (req, res) => {
+    try {
+      await characterModel.findByIdAndUpdate(req.params.id, req.body)
+      await characterModel.save()
+      res.send(character)
+    } catch (err) {
+      res.status(500).send(err)
+    }
+  })
+
+// Delete
+router.delete('/characters/:id', async (req, res) => {
+    try {
+      const character = await characterModel.findByIdAndDelete(req.params.id)
+  
+      if (!character) res.status(404).send("No item found")
+      res.status(200).send()
+    } catch (err) {
+      res.status(500).send(err)
+    }
+  })
 
 module.exports = router
