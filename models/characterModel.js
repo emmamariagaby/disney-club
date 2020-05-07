4c4b1a1e007b5005c9a5c81a37820fc171bebed0
Louise Bäckström <backstromlouise@gmail.com>
2020-05-07 12:37:50 +0200

    added characters to database

--- models/characterModel.js
+++ models/characterModel.js
@@ -1,24 +1,9 @@
 const mongoose = require('mongoose')
 
 let CharacterSchema = new mongoose.Schema({
-  title: {
-    type: String,
-  },
-  info: {
     name: String,
     movie: String,
-    bestFrient: String,
-  },
-  description: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-  },
-  img: {
-    data: Buffer,
-    contentType: String,
-  },
+    bestFriend: String
 })
 
 const Character = mongoose.model('Character', CharacterSchema)
