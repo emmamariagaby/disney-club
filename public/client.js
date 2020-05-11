6ad4fd5875bcfc4051cd7d936baf91b9426b74e9
Embla <emblajonsson@gmail.com>
2020-05-11 11:20:41 +0200

    delete

--- public/client.js
+++ public/client.js
@@ -6,7 +6,7 @@ fetch("/characters").then((response) => {
 
 function allCharacters(characters) {
     let allCharactersContainer = document.getElementById("getAllCharacters")
-    
+
     characters.forEach(character => {
         let characterId = document.createElement("p")
         characterId.innerText = "Disney Id : " + character._id
@@ -19,7 +19,7 @@ function allCharacters(characters) {
 
         let characterDiv = document.createElement("div")
         characterDiv.classList.add("all")
-        
+
         characterDiv.appendChild(characterId)
         characterDiv.appendChild(characterName)
         characterDiv.appendChild(characterMovie)
@@ -44,21 +44,22 @@ function createNew(event) {
     }
 
     fetch("/characters", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(character)
-})
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(character)
+    })
 
-let createContainer = document.getElementById("message")
-   
+    let createContainer = document.getElementById("message")
+
     let message = document.createElement("h4")
     message.innerText = "En ny Disneyfigur har lagts till"
-    createContainer.appendChild(message) 
+    createContainer.appendChild(message)
 
-    setTimeout(function () { 
-        window.location.reload() }, 1500)
+    setTimeout(function () {
+        window.location.reload()
+    }, 1500)
 }
 
 
@@ -75,28 +76,61 @@ function update(event) {
         const [key, value] = pair
         updatedCharacter[key] = value
     }
-    const id = document.getElementById("updateId").value 
-    const name = document.getElementById("updateName").value 
+    const id = document.getElementById("updateId").value
+    const name = document.getElementById("updateName").value
     fetch("/characters/" + id, {
-    method: 'PUT',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(updatedCharacter)
-})
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedCharacter)
+    })
 
-let createContainer = document.getElementById("updateMessage")
-   
-let updateMessage = document.createElement("h4")
-message.innerText = name + " har uppdaterats"
-createContainer.appendChild(updateMessage) 
+    let createContainer = document.getElementById("updateMessage")
 
-setTimeout(function () { 
-    window.location.reload() }, 1500)
+    let updateMessage = document.createElement("h4")
+    message.innerText = name + " har uppdaterats"
+    createContainer.appendChild(updateMessage)
+
+    setTimeout(function () {
+        window.location.reload()
+    }, 1500)
 
 }
 
+const deleteform = document.getElementById('deleteCharacter')
+deleteform.addEventListener('submit', deleteCharacter)
+function deleteCharacter(event) {
+    event.stopPropagation()
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const deletedCharacter = {}
+    for (const pair of formData.entries()) {
+        const [key, value] = pair
+        deletedCharacter[key] = value
+    }
+    const id = document.getElementById("deleteId").value
+    fetch("/characters/" + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify()
+    })
+
+    let deletecontainer = document.getElementById("deleteMessage")
+
+    let deleteMessage = document.createElement("h4")
+    message.innerText = id + " har raderats"
+    container.appendChild(deleteMessage)
+
+    setTimeout(function () {
+        window.location.reload()
+    }, 1500)
+}
+
 //form, login and create
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+$('.message a').click(function () {
+    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
 });
