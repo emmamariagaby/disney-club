ec1ef38dea31beaa2ef8983655fe25127e467b70
Louise Bäckström <backstromlouise@gmail.com>
2020-05-12 10:21:43 +0200

    added som more css, added a separate css file for login and register

--- public/clientMyCharacters.js
+++ public/clientMyCharacters.js
@@ -58,7 +58,7 @@ function update(event) {
     let createContainer = document.getElementById("updateMessage")
 
     let updateMessage = document.createElement("h4")
-    message.innerText = name + " har uppdaterats"
+    updateMessage.innerText = name + " har uppdaterats"
     createContainer.appendChild(updateMessage)
 
     setTimeout(function () {
@@ -91,10 +91,45 @@ function deleteCharacter(event) {
     let deletecontainer = document.getElementById("deleteMessage")
 
     let deleteMessage = document.createElement("h4")
-    message.innerText = id + " har raderats"
-    container.appendChild(deleteMessage)
+    deleteMessage.innerText = id + " har raderats"
+    deletecontainer.appendChild(deleteMessage)
 
     setTimeout(function () {
         window.location.reload()
     }, 1500)
 }
+
+fetch("/characters").then((response) => {
+    return response.json()
+}).then((characters) => {
+    allCharacters(characters)
+})
+
+function allCharacters(characters) {
+    let allCharactersContainer = document.getElementById("getAllCharacters")
+
+    characters.forEach(character => {
+        let characterHeadline = document.createElement("h4")
+        characterHeadline.innerText = character.name + " in " + character.movie
+        let characterName = document.createElement("p")
+        characterName.innerText = "Disney Character: " + character.name
+        let characterMovie = document.createElement("p")
+        characterMovie.innerText = "Disney Movie: " + character.movie
+        let characterBestFriend = document.createElement("p")
+        characterBestFriend.innerText = "Best Friend: " + character.bestFriend
+        let characterId = document.createElement("p")
+        characterId.innerText = "Disney Id : " + character._id
+
+        let characterDiv = document.createElement("div")
+        characterDiv.classList.add("all")
+
+        characterDiv.appendChild(characterHeadline)
+        characterDiv.appendChild(characterName)
+        characterDiv.appendChild(characterMovie)
+        characterDiv.appendChild(characterBestFriend)
+        characterDiv.appendChild(characterId)
+
+        allCharactersContainer.appendChild(characterDiv)
+    })
+}
+
