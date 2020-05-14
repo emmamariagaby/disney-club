7aeee364a3bdfe376d7fce05deea73201b65db35
emmamariagaby <emmamariagaby@gmail.com>
2020-05-14 14:25:52 +0200

    added some fix

--- connect.js
+++ connect.js
@@ -6,5 +6,5 @@ mongoose.connect("mongodb://localhost:27017/disney-club", options)
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
-  console.log("we're connected!")
+  console.log("we're connected to MongoDB!")
 })
--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 let UserSchema = new mongoose.Schema({
   username: {
-    type: String
+    type: String,
+    required: true
   },
   password: {
     type: String,
