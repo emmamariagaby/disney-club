4439c00523fed0fd25f785ee18f5e0bfb719a9bc
Louise Bäckström <backstromlouise@gmail.com>
2020-05-14 15:37:17 +0200

    added loginUser() in clientLogin.js

--- public/clientLogin.js
+++ public/clientLogin.js
@@ -11,4 +11,33 @@ var win = window.open('/#login');
 // redirect from login to website
 function openSiteLogin(url) {
     var win2 = window.open('/index1.html');
+}
+
+const form = document.getElementById('login-form')
+form.addEventListener('submit', loginUser)
+
+function loginUser(event) {
+    event.stopPropagation()
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const user = {
+        username: "",
+        password: ""
     }
+    for (const pair of formData.entries()) {
+        const [key, value] = pair
+        user[key] = value
+    }
+
+    fetch("/users/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+       
+    })
+
+    console.log(user.username + " is logged in")
+}
